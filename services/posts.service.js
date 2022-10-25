8b2e34b026f7b8af7863b2a3da298bf07a60008a
leejun <koreadlwns@naver.com>
2022-10-25 14:45:31 +0900

    오늘까지 수정한 파일입니다.

---
+++ services/posts.service.js
@@ -0,0 +1,99 @@
+const PostRepository = require("../repositories/posts.repository");
+
+class PostService {
+  postRepository = new PostRepository();
+
+  findAllPost = async () => {
+    // 저장소(Repository)에게 데이터를 요청합니다.
+    const allPost = await this.postRepository.findAllPost();
+
+    // 호출한 Post들을 가장 최신 게시글 부터 정렬합니다.
+    allPost.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    });
+
+    // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공합니다.
+    return allPost.map((post) => {
+      return {
+        postId: post.postId,
+        nickname: post.nickname,
+        title: post.title,
+        createdAt: post.createdAt,
+        updatedAt: post.updatedAt,
+      };
+    });
+  };
+
+  findPostById = async (postId) => {
+    const findPost = await this.postRepository.findPostById(postId);
+    // console.log(findPost)
+    const result = {
+      postId: findPost.postId,
+      nickname: findPost.nickname,
+      userKey: findPost.userKey,
+      title: findPost.title,
+      content: findPost.content,
+      createdAt: findPost.createdAt,
+      updatedAt: findPost.updatedAt,
+    };
+    return result;
+  };
+
+  createPost = async (nickname, userKey, title, content) => {
+    // 저장소(Repository)에게 데이터를 요청합니다.
+    const createPostData = await this.postRepository.createPost(
+      nickname,
+      userKey,
+      title,
+      content
+    );
+
+    // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공합니다.
+    return {
+      postId: createPostData.postId,
+      userKey: createPostData.userKey,
+      nickname: createPostData.nickname,
+      title: createPostData.title,
+      content: createPostData.content,
+      createdAt: createPostData.createdAt,
+      updatedAt: createPostData.updatedAt,
+    };
+  };
+
+  updatePost = async (postId, title, content) => {
+    const findPost = await this.postRepository.findPostById(postId);
+    console.log(findPost)
+    if (!findPost) throw new Error("Post doesn't exist");
+
+    await this.postRepository.updatePost(postId, title, content);
+
+    const updatePost = await this.postRepository.findPostById(postId);
+
+    return {
+      postId: updatePost.postId,
+      nickname: updatePost.nickname,
+      title: updatePost.title,
+      content: updatePost.content,
+      createdAt: updatePost.createdAt,
+      updatedAt: updatePost.updatedAt,
+    };
+  };
+
+  deletePost = async (postId) => {
+    const findPost = await this.postRepository.findPostById(postId);
+    if (!findPost) throw new Error("Post doesn't exist");
+
+    await this.postRepository.deletePost(postId);
+
+    return {
+      postId: findPost.postId,
+      nickname: findPost.nickname,
+      title: findPost.title,
+      content: findPost.content,
+      createdAt: findPost.createdAt,
+      updatedAt: findPost.updatedAt,
+    };
+  };
+}
+
+module.exports = PostService;
